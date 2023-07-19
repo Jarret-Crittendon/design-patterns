1b59e8bb31be8e588156bc915a298fb1b017bd18
Jarret Crittendon <jarret-dev@proton.me>
2023-07-19 02:54:54 -0400

    First

---
+++ decorator/src/Decorator.hpp
@@ -0,0 +1,8 @@
+// Author: Jarret Crittendon
+// 2023 JUL 19
+// Overview:  Challenge Problem for LinkedIn Learning Course,
+// "Programming Foundations: Design Patterns" - implementing the Observer
+// pattern
+
+// ThinCrustPizza, ThickCrustPizza
+// Toppings: Cheese, Olives, Peppers
