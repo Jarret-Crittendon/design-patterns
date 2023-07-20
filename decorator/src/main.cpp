383aede34c7cf1c360ce0208b316ba0922521243
Jarret Crittendon <jarret-dev@proton.me>
2023-07-20 19:50:08 -0400

    Added more lines to main

--- decorator/src/main.cpp
+++ decorator/src/main.cpp
@@ -10,4 +10,14 @@ int main() {
 
     std::cout << "I'm ordering a " << pizza->get_desc() << std::endl;
     std::cout << "It costs $" << pizza->cost() << std::endl;
+
+    std::cout << std::endl;
+
+    std::shared_ptr<Pizza> pie = std::make_shared<ThinCrustPizza>();
+    pie = std::make_shared<Peppers>(pie);
+    pie = std::make_shared<Olives>(pie);
+    pie = std::make_shared<Peppers>(pie);
+
+    std::cout << "I'm also ordering a " << pie->get_desc() << std::endl;
+    std::cout << "It costs $" << pie->cost() << std::endl;
 }
